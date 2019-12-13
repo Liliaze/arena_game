ea89c3b45f94d749a0978b79b5b0c63e874f2252
Liliaze <diana.boudy@gmail.com>
2019-12-13 10:55:38 +0100

    init and create players in start map, fix bug if random position is take by an another weapon or player.

--- js/main.js
+++ js/main.js
@@ -3,16 +3,18 @@
 var boardGame = [];
 var boardGameElement = null;
 var weapons = [];
+var players = [];
 
 $(document).ready(function () {
 
- addTitleContent();
+ initTitleContent();
  initWeapons();
+ initPlayers();
  newBoardGame();
 });
 
 
-function addTitleContent() {
+function initTitleContent() {
  $('#firstTitle').html('ARENA GAME');
  $('#secondTitle').html('Fight for your life');
  $('#scoreP1').html('Player 1 : Arya');
@@ -22,15 +24,26 @@ function addTitleContent() {
 }
 
 function initWeapons() {
- weapons[0] = createWeapon(10, 1, 10, 0, "sword", "image/sword.jpg");
- weapons[1] = createWeapon(50, 4, 1, 2, "bomb", "image/bomb.jpg");
- weapons[3] = createWeapon(20, 10, 5, 0, "shuriken", "image/shuriken.jpg");
- weapons[4] = createWeapon(25, 1, 8, 0, "hammer", "image/hammer.jpg");
+ weapons.push(createWeapon(10, 1, 10, 0, "sword", "image/sword.jpg"));
+ weapons.push(createWeapon(50, 4, 1, 2, "bomb", "image/bomb.jpg"));
+ weapons.push(createWeapon(20, 10, 5, 0, "shuriken", "image/shuriken.jpg"));
+ weapons.push(createWeapon(25, 1, 8, 0, "hammer", "image/hammer.jpg"));
 };
+
+function initPlayers() {
+ players.push(createPlayer(100, 'John', "image/blackPlayer.jpg"));
+ players.push(createPlayer(100, 'Arya', "image/bluePlayer.jpg"));
+}
 
 function createWeapon(damage, distance, durability, delay, name, imageSrc) {
  return {
   damage, durability, delay, name, imageSrc
+ }
+}
+
+function createPlayer(life, name, imageSrc) {
+ return {
+  life, name, imageSrc
  }
 }
 
@@ -39,7 +52,7 @@ function newBoardGame() {
  const maxNumber = 20;
  const minNumber = 10;
  boardGame = [];
- boardGameSize = 15; //Math.floor(Math.random() * (maxNumber + 1) + minNumber);
+ boardGameSize = 12; //Math.floor(Math.random() * (maxNumber + 1) + minNumber);
  console.log('boardGameSize = ' + boardGameSize);
  numberOfBlock = boardGameSize * boardGameSize * 0.1 * 2;
  $('<table>').appendTo('#boardGame');
@@ -62,18 +75,35 @@ function newBoardGame() {
   }
  }
 
- weapons.forEach(
-  function (weapon) {
-   const randomPositionRow = Math.floor(Math.random() * (boardGameSize + 1)).toString();
-   const randomPositionColumn = Math.floor(Math.random() * (boardGameSize + 1)).toString();
-   console.log('random =' + randomPositionRow + "," + randomPositionColumn);
-   $('img').each(function () {
-    if ($(this).attr('row') == randomPositionRow && $(this).attr('column') == randomPositionColumn) {
-     //$(this).click(function () {
-     $(this).replaceWith($('<img>').attr('src', weapon.imageSrc).addClass(weapon.name));
-     //});
+ for (let i = 0; i < weapons.length; i++) {
+  const randomPositionRow = Math.floor(Math.random() * boardGameSize).toString();
+  const randomPositionColumn = Math.floor(Math.random() * boardGameSize).toString();
+  console.log('random =' + randomPositionRow + "," + randomPositionColumn + "," + i + ":" + weapons[i].name);
+  $('img').each(function () {
+   if ($(this).attr('row') == randomPositionRow && $(this).attr('column') == randomPositionColumn) {
+    if ($(this).hasClass('ground')) {
+     $(this).replaceWith($('<img>').attr('row', $(this).attr('row')).attr('column', $(this).attr('column')).attr('src', weapons[i].imageSrc).addClass(weapons[i].name));
+    } else {
+     i--;
+     return;
     }
-   });
+   }
   });
+ }
 
+ for (let i = 0; i < players.length; i++) {
+  const randomPositionRow = Math.floor(Math.random() * boardGameSize).toString();
+  const randomPositionColumn = Math.floor(Math.random() * boardGameSize).toString();
+  console.log('random =' + randomPositionRow + "," + randomPositionColumn + "," + i + "," + players[i].name);
+  $('img').each(function () {
+   if ($(this).attr('row') == randomPositionRow && $(this).attr('column') == randomPositionColumn) {
+    if ($(this).hasClass('ground')) {
+     $(this).replaceWith($('<img>').attr('row', $(this).attr('row')).attr('column', $(this).attr('column')).attr('src', players[i].imageSrc).attr('playerNumber', i).addClass('player'));
+    } else {
+     i--;
+     return;
+    }
+   }
+  });
+ };
 }
